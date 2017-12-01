84b69be012a60c9b7167c995abc8958dde7ee2c1
Yuuki Sone <yuki_sone@dwango.co.jp>
2017-12-02 04:20:51 +0900

    change clk

--- main.cpp
+++ main.cpp
@@ -7,10 +7,11 @@ using namespace std;
 static const int GPIO_VCC = 26; // GREEN
 static const int GPIO_DAT = 19; // RED
 static const int GPIO_P_S = 13; // WHITE
-static const int GPIO_CLK = 6; // YELLOW
+static const int GPIO_CLK = 20; // YELLOW
 
-void init() {
-}
+// void aFunction(int gpio, int level, uint32_t tick) {
+//   printf("GPIO %d became %d at %d", gpio, level, tick);
+// }
 
 int main() {
   if (gpioInitialise() < 0) return 1;
@@ -23,6 +24,8 @@ int main() {
   cout << "DAT   " << gpioRead(GPIO_DAT) << endl;
   cout << "CLK   " << gpioRead(GPIO_CLK) << endl;
   cout << "P/S   " << gpioRead(GPIO_P_S) << endl;
+
+  // gpioSetAlertFunc(4, aFunction);
 
   gpioTerminate();
   return 0;
